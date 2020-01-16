6e02a90be8b47b30e87b031869ce4d6cfd536f31
Plamen Georgiev <pgeorgiev.dev@gmail.com>
2020-01-16 15:00:15 +0200

    Added new folder and file full-width template

---
+++ templates/full-width.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * Template Name: Full-Width template
+ *
+ * Custom homepage template for the theme
+ * 
+ * @since  v1.0.0
+ * @package Catalina
+ */
+get_header(); ?>
+
+<?php // Custom page template structure ?>
+
+<?php get_footer(); ?>
